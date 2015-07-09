419f2128212197ece4ed39857512b27c5d0cb699
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2015-07-10 11:42:22 +1200

    Fix test for bounds message

--- test/com/rtg/vcf/VcfFilterCliTest.java
+++ test/com/rtg/vcf/VcfFilterCliTest.java
@@ -228,34 +228,34 @@ public class VcfFilterCliTest extends AbstractCliTest {
 
       assertTrue(out.delete());
       err = checkHandleFlagsErr("-i", in.getPath(), "-o", out.getPath(), "-p", "-1.0");
-      TestUtils.containsAll(TestUtils.unwrap(err), "The specified flag \"--Xmin-posterior-score\" has invalid value \"" + -1.0 + "\". It should be greater than or equal to 0.0 and less than or equal to Infinity.");
+      TestUtils.containsAll(TestUtils.unwrap(err), "The specified flag \"--Xmin-posterior-score\" has invalid value \"" + -1.0 + "\". It should be greater than or equal to 0.0 and less than Infinity.");
 
       err = checkHandleFlagsErr("-i", in.getPath(), "-o", out.getPath(), "-p", "1.0", "-P", "0.0");
-      TestUtils.containsAll(TestUtils.unwrap(err), "The specified flag \"--Xmax-posterior-score\" has invalid value \"" + 0.0 + "\". It should be greater than or equal to " + 1.0 + " and less than or equal to Infinity.");
+      TestUtils.containsAll(TestUtils.unwrap(err), "The specified flag \"--Xmax-posterior-score\" has invalid value \"" + 0.0 + "\". It should be greater than or equal to " + 1.0 + " and less than Infinity.");
 
       err = checkHandleFlagsErr("-i", in.getPath(), "-o", out.getPath(), "-Q", "-1.0");
-      TestUtils.containsAll(TestUtils.unwrap(err), "The specified flag \"--max-quality\" has invalid value \"" + -1.0 + "\". It should be greater than or equal to 0.0 and less than or equal to Infinity.");
+      TestUtils.containsAll(TestUtils.unwrap(err), "The specified flag \"--max-quality\" has invalid value \"" + -1.0 + "\". It should be greater than or equal to 0.0 and less than Infinity.");
 
       err = checkHandleFlagsErr("-i", in.getPath(), "-o", out.getPath(), "-q", "-1.0");
-      TestUtils.containsAll(TestUtils.unwrap(err), "The specified flag \"--min-quality\" has invalid value \"" + -1.0 + "\". It should be greater than or equal to 0.0 and less than or equal to Infinity.");
+      TestUtils.containsAll(TestUtils.unwrap(err), "The specified flag \"--min-quality\" has invalid value \"" + -1.0 + "\". It should be greater than or equal to 0.0 and less than Infinity.");
 
       err = checkHandleFlagsErr("-i", in.getPath(), "-o", out.getPath(), "-q", "1.0", "-Q", "0.0");
-      TestUtils.containsAll(TestUtils.unwrap(err), "The specified flag \"--max-quality\" has invalid value \"" + 0.0 + "\". It should be greater than or equal to " + 1.0 + " and less than or equal to Infinity.");
+      TestUtils.containsAll(TestUtils.unwrap(err), "The specified flag \"--max-quality\" has invalid value \"" + 0.0 + "\". It should be greater than or equal to " + 1.0 + " and less than Infinity.");
 
       err = checkHandleFlagsErr("-i", in.getPath(), "-o", out.getPath(), "-Q", "-1.0");
-      TestUtils.containsAll(TestUtils.unwrap(err), "The specified flag \"--max-quality\" has invalid value \"" + -1.0 + "\". It should be greater than or equal to 0.0 and less than or equal to Infinity.");
+      TestUtils.containsAll(TestUtils.unwrap(err), "The specified flag \"--max-quality\" has invalid value \"" + -1.0 + "\". It should be greater than or equal to 0.0 and less than Infinity.");
 
       err = checkHandleFlagsErr("-i", in.getPath(), "-o", out.getPath(), "-G", "-1.0");
-      TestUtils.containsAll(TestUtils.unwrap(err), "The specified flag \"--max-genotype-quality\" has invalid value \"" + -1.0 + "\". It should be greater than or equal to 0.0 and less than or equal to Infinity.");
+      TestUtils.containsAll(TestUtils.unwrap(err), "The specified flag \"--max-genotype-quality\" has invalid value \"" + -1.0 + "\". It should be greater than or equal to 0.0 and less than Infinity.");
 
       err = checkHandleFlagsErr("-i", in.getPath(), "-o", out.getPath(), "-g", "-1.0");
-      TestUtils.containsAll(TestUtils.unwrap(err), "The specified flag \"--min-genotype-quality\" has invalid value \"" + -1.0 + "\". It should be greater than or equal to 0.0 and less than or equal to Infinity.");
+      TestUtils.containsAll(TestUtils.unwrap(err), "The specified flag \"--min-genotype-quality\" has invalid value \"" + -1.0 + "\". It should be greater than or equal to 0.0 and less than Infinity.");
 
       err = checkHandleFlagsErr("-i", in.getPath(), "-o", out.getPath(), "-g", "1.0", "-G", "0.0");
-      TestUtils.containsAll(TestUtils.unwrap(err), "The specified flag \"--max-genotype-quality\" has invalid value \"" + 0.0 + "\". It should be greater than or equal to " + 1.0 + " and less than or equal to Infinity.");
+      TestUtils.containsAll(TestUtils.unwrap(err), "The specified flag \"--max-genotype-quality\" has invalid value \"" + 0.0 + "\". It should be greater than or equal to " + 1.0 + " and less than Infinity.");
 
       err = checkHandleFlagsErr("-i", in.getPath(), "-o", out.getPath(), "-G", "-1.0");
-      TestUtils.containsAll(TestUtils.unwrap(err), "The specified flag \"--max-genotype-quality\" has invalid value \"" + -1.0 + "\". It should be greater than or equal to 0.0 and less than or equal to Infinity.");
+      TestUtils.containsAll(TestUtils.unwrap(err), "The specified flag \"--max-genotype-quality\" has invalid value \"" + -1.0 + "\". It should be greater than or equal to 0.0 and less than Infinity.");
 
       err = checkHandleFlagsErr("-i", in.getPath(), "-o", out.getPath(), "-d", "-1");
       TestUtils.containsAll(TestUtils.unwrap(err), "The specified flag \"--min-read-depth\" has invalid value \"-1\". It should be greater than or equal to 0.");
