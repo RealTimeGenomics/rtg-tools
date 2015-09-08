2f1fa3711d3e9e93e00a202919456afaaf95e631
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2015-09-08 13:25:25 +1200

    Safer missing AD/alleles handling

--- src/com/rtg/vcf/annotation/ContraryObservationFractionAnnotation.java
+++ src/com/rtg/vcf/annotation/ContraryObservationFractionAnnotation.java
@@ -96,12 +96,11 @@ public class ContraryObservationFractionAnnotation extends AbstractDerivedFormat
 
   private int[] ad(final int[] res, final VcfRecord record, final int sample) {
     final String ad = record.getSampleString(sample, VcfUtils.FORMAT_ALLELIC_DEPTH);
-    if (ad == null) {
-      return null;
-    }
-    final String[] adSplit = StringUtils.split(ad, ',');
-    for (int k = 0; k < res.length; k++) {
-      res[k] += Integer.parseInt(adSplit[k]);
+    if (ad != null) {
+      final String[] adSplit = StringUtils.split(ad, ',');
+      for (int k = 0; k < res.length; k++) {
+        res[k] += Integer.parseInt(adSplit[k]);
+      }
     }
     return res;
   }
@@ -120,11 +119,10 @@ public class ContraryObservationFractionAnnotation extends AbstractDerivedFormat
 
   private boolean[] alleles(final boolean[] res, final VcfRecord record, final int sampleNumber) {
     final int[] originalGt = VcfUtils.getValidGt(record, sampleNumber);
-    if (originalGt == null) {
-      return null;
-    }
-    for (final int allele : originalGt) {
-      res[allele] = true;
+    if (originalGt != null) {
+      for (final int allele : originalGt) {
+        res[allele] = true;
+      }
     }
     return res;
   }
