e6b087310033625420e59cfcd869876b39119125
Len Trigg <len@realtimegenomics.com>
2016-02-04 11:23:54 +1300

    rocplot: Got a talkback from yet another way that Java can error when the environment would not allow a GUI. Let's just treat *any* error in the isReallyHeadless test as such.

--- src/com/rtg/graph/RocPlotCli.java
+++ src/com/rtg/graph/RocPlotCli.java
@@ -157,7 +157,7 @@ public class RocPlotCli extends AbstractCli {
     try {
       final GraphicsDevice[] screenDevices = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices();
       return screenDevices == null || screenDevices.length == 0;
-    } catch (InternalError | UnsatisfiedLinkError e) {
+    } catch (Error e) {
       Diagnostic.error(e.getMessage());
       return true;
     } catch (HeadlessException e) {
