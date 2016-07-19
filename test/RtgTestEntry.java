c86e193cb35134e3c46a03ffb2f7a851e4be860b
Kurt Gaastra <kurt.gaastra@realtimegenomics.com>
2016-07-19 14:25:33 +1200

    Add test timing, output cleanup

--- test/RtgTestEntry.java
+++ test/RtgTestEntry.java
@@ -29,12 +29,19 @@
  */
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import org.apache.commons.lang.StringUtils;
 import org.junit.internal.RealSystem;
 import org.junit.internal.TextListener;
+import org.junit.runner.Description;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
+import org.junit.runner.notification.RunListener;
 import org.junit.runners.model.InitializationError;
 
 /**
@@ -51,11 +58,72 @@ import org.junit.runners.model.InitializationError;
  * </pre>
  */
 public class RtgTestEntry {
+
+  private static final boolean COLLECT_TIMINGS = Boolean.valueOf(System.getProperty("junit.timing", "false"));
+  private static final int TESTS_PER_ROW = 120;
+  private static final long LONG_TEST_THRESHOLD = 200;
+  private static final int TIMING_WIDTH = 120;
+
+  private static class TimingListener extends RunListener {
+    private final Map<String, Long> mTimings = new HashMap<>();
+    private long mStart;
+    private String mTestName = null;
+    @Override
+    public void testStarted(Description description) throws Exception {
+      mStart = System.currentTimeMillis();
+      mTestName = testName(description);
+    }
+
+    private String testName(Description description) {
+      return description.getClassName() + "." + description.getMethodName();
+    }
+
+    @Override
+    public void testFinished(Description description) throws Exception {
+      final long end = System.currentTimeMillis();
+      if (mTestName.equals(testName(description))) {
+        mTimings.put(testName(description), end - mStart);
+      } else {
+        System.err.println("Can't time " + mTestName);
+      }
+    }
+
+    static String timingFormatter(String name, long time, int width) {
+      return name + StringUtils.leftPad(String.valueOf(time), Math.max(0, width - name.length()));
+    }
+
+    Map<String, Long> getTimings(long threshold) {
+      final LinkedHashMap<String, Long> map = new LinkedHashMap<>();
+      final List<Map.Entry<String, Long>> sorted = new ArrayList<>();
+      sorted.addAll(mTimings.entrySet().stream().filter(e -> e.getValue() > threshold).collect(Collectors.toCollection(ArrayList::new)));
+      sorted.sort((a, b) -> a.getValue() > b.getValue() ? -1 : a.getValue().equals(b.getValue()) ? 0 : 1 );
+      sorted.forEach(e -> map.put(e.getKey(), e.getValue()));
+      return map;
+    }
+  }
+
   @SuppressWarnings("unchecked")
   public static void main(final String[] args) throws ClassNotFoundException, InitializationError {
     final JUnitCore jUnitCore = new JUnitCore();
     jUnitCore.addListener(new TextListener(new RealSystem()));
-    List<Result> results = new ArrayList<>();
+    final TimingListener timing;
+    if (COLLECT_TIMINGS) {
+      timing =new TimingListener();
+      jUnitCore.addListener(timing);
+    } else {
+      timing = null;
+    }
+    jUnitCore.addListener(new RunListener() {
+      int mTestCount = 0;
+      @Override
+      public void testFinished(Description description) throws Exception {
+        mTestCount++;
+        if (mTestCount % TESTS_PER_ROW == 0) {
+          System.out.println();
+        }
+      }
+    });
+    final List<Result> results = new ArrayList<>();
     if (args.length > 0) {
       for (final String arg : args) {
         final Class<?> klass = ClassLoader.getSystemClassLoader().loadClass(arg);
@@ -66,6 +134,17 @@ public class RtgTestEntry {
       final Class<?>[] classes = getClasses();
       results.add(jUnitCore.run(classes));
     }
+    if (timing != null) {
+      final Map<String, Long> timings = timing.getTimings(LONG_TEST_THRESHOLD);
+      if (timings.size() > 1) {
+        System.out.println();
+        System.out.println("Long tests");
+        for (Map.Entry<String, Long> entry : timings.entrySet()) {
+          System.out.println(TimingListener.timingFormatter(entry.getKey(), entry.getValue(), TIMING_WIDTH));
+        }
+      }
+    }
+
     for (Result result : results) {
       if (!result.wasSuccessful()) {
         System.exit(1);
