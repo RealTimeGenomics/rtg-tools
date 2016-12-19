d78e236c8c8485fc0eef635bd05ddfcf34a26106
Kurt Gaastra <kurt.gaastra@realtimegenomics.com>
2016-12-19 16:24:48 +1300

    Test case for end of sequence stacked regions

--- test/com/rtg/sam/SamClosedFileReaderTest.java
+++ test/com/rtg/sam/SamClosedFileReaderTest.java
@@ -96,14 +96,7 @@ public class SamClosedFileReaderTest extends TestCase {
       final SamRegionRestriction rr = new SamRegionRestriction("simulatedSequence2", 10000, 20000);
       final Iterator<SAMRecord> norm = normalReader.query(rr.getSequenceName(), rr.getStart() + 1, rr.getEnd(), false);
       final ReferenceRanges<String> ranges = SamRangeUtils.createExplicitReferenceRange(normalReader.getFileHeader(), rr);
-      try (final RecordIterator<SAMRecord> closed = new SamClosedFileReader(sam, ranges, null, normalReader.getFileHeader())) {
-        while (norm.hasNext() && closed.hasNext()) {
-          final SAMRecord a = norm.next();
-          final SAMRecord b = closed.next();
-          assertEquals(a.getSAMString(), b.getSAMString());
-        }
-        assertEquals(norm.hasNext(), closed.hasNext());
-      }
+      checkRecords(sam, normalReader, norm, ranges);
     }
   }
 
@@ -129,6 +122,38 @@ public class SamClosedFileReaderTest extends TestCase {
       check2(bam, bam);
     } finally {
       assertTrue(FileHelper.deleteAll(dir));
+    }
+  }
+
+  public void testRegionsAtEndOfSequence() throws IOException {
+    // This test simulates an error that was occuring in coverage/sammerge when you have two regions at the end of a
+    // and the second to last region is overlapped by the final read in the sequence.
+
+    final File dir = FileUtils.createTempDir("closedfilereader", "test");
+    try {
+      final File sam = FileHelper.resourceToFile("com/rtg/sam/resources/multiRegionSequenceEnd.sam.gz", new File(dir, "sam.sam.gz"));
+      final File bam = new File(dir, "bam.bam");
+      SamUtilsTest.convertSamToBam(bam, BamIndexer.indexFileName(bam), sam);
+      try (SamReader normalReader = SamUtils.makeSamReader(bam)) {
+        final SamRegionRestriction r1 = new SamRegionRestriction("chr7", 159334000, 159335000);
+        final SamRegionRestriction r2 = new SamRegionRestriction("chr7", 159335000, 159335001);
+        final Iterator<SAMRecord> norm = normalReader.query(r1.getSequenceName(), r1.getStart(), r2.getEnd(), false);
+        final ReferenceRanges<String> ranges = SamRangeUtils.createExplicitReferenceRange(normalReader.getFileHeader(), r1, r2);
+        checkRecords(bam, normalReader, norm, ranges);
+      }
+    } finally {
+      assertTrue(FileHelper.deleteAll(dir));
+    }
+  }
+
+  private void checkRecords(File bam, SamReader normalReader, Iterator<SAMRecord> norm, ReferenceRanges<String> ranges) throws IOException {
+    try (final RecordIterator<SAMRecord> closed = new SamClosedFileReader(bam, ranges, null, normalReader.getFileHeader())) {
+      while (norm.hasNext() && closed.hasNext()) {
+        final SAMRecord a = norm.next();
+        final SAMRecord b = closed.next();
+        assertEquals(a.getSAMString(), b.getSAMString());
+      }
+      assertEquals(norm.hasNext(), closed.hasNext());
     }
   }
 }
