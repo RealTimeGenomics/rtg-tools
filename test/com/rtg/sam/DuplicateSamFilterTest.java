c7a787ff97faca455caf57f3c8a0281961a29944
Len Trigg <len@realtimegenomics.com>
2018-03-13 11:59:08 +1300

    Avoid use of deprecated method

--- test/com/rtg/sam/DuplicateSamFilterTest.java
+++ test/com/rtg/sam/DuplicateSamFilterTest.java
@@ -50,15 +50,15 @@ public class DuplicateSamFilterTest extends TestCase {
 
     assertTrue(dsf.acceptRecord(rec1));
 
-    rec1.setNotPrimaryAlignmentFlag(true);
+    rec1.setSecondaryAlignment(true);
 
     final SAMRecord rec2 = new SAMRecord(null);
     rec2.setReadName("Sabu");
-    rec2.setNotPrimaryAlignmentFlag(true);
+    rec2.setSecondaryAlignment(true);
 
     final SAMRecord rec3 = new SAMRecord(null);
     rec3.setReadName("IronSheik");
-    rec3.setNotPrimaryAlignmentFlag(true);
+    rec3.setSecondaryAlignment(true);
 
     assertTrue(dsf.acceptRecord(rec1));
     assertTrue(dsf.acceptRecord(rec2));
@@ -84,37 +84,37 @@ public class DuplicateSamFilterTest extends TestCase {
 
     assertTrue(dsf.acceptRecord(rec1f));
 
-    rec1f.setNotPrimaryAlignmentFlag(true);
+    rec1f.setSecondaryAlignment(true);
 
     final SAMRecord rec2f = new SAMRecord(null);
     rec2f.setReadPairedFlag(true);
     rec2f.setFirstOfPairFlag(true);
     rec2f.setReadName(secondReadName);
-    rec2f.setNotPrimaryAlignmentFlag(true);
+    rec2f.setSecondaryAlignment(true);
 
     final SAMRecord rec3fdup = new SAMRecord(null);
     rec3fdup.setReadPairedFlag(true);
     rec3fdup.setFirstOfPairFlag(true);
     rec3fdup.setReadName(firstReadName);
-    rec3fdup.setNotPrimaryAlignmentFlag(true);
+    rec3fdup.setSecondaryAlignment(true);
 
     final SAMRecord rec1s = new SAMRecord(null);
     rec1s.setReadName(firstReadName);
     rec1s.setReadPairedFlag(true);
     rec1s.setFirstOfPairFlag(false);
-    rec1s.setNotPrimaryAlignmentFlag(true);
+    rec1s.setSecondaryAlignment(true);
 
     final SAMRecord rec2s = new SAMRecord(null);
     rec2s.setReadPairedFlag(true);
     rec2s.setFirstOfPairFlag(false);
     rec2s.setReadName(secondReadName);
-    rec2s.setNotPrimaryAlignmentFlag(true);
+    rec2s.setSecondaryAlignment(true);
 
     final SAMRecord rec3sdup = new SAMRecord(null);
     rec3sdup.setReadPairedFlag(true);
     rec3sdup.setFirstOfPairFlag(false);
     rec3sdup.setReadName(firstReadName);
-    rec3sdup.setNotPrimaryAlignmentFlag(true);
+    rec3sdup.setSecondaryAlignment(true);
 
 
     assertTrue(dsf.acceptRecord(rec1f));
