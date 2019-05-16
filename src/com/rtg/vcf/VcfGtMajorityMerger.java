5db0de82c29e4bcc6ff40b88bd0a681864b50078
Len Trigg <len@realtimegenomics.com>
2019-05-16 14:04:27 +1200

    Initial GT majority vote mode for VCF merging.

---
+++ src/com/rtg/vcf/VcfGtMajorityMerger.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2014. Real Time Genomics Limited.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.rtg.vcf;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+import com.rtg.util.MultiSet;
+import com.rtg.util.diagnostic.Diagnostic;
+import com.rtg.util.intervals.SequenceNameLocusSimple;
+import com.rtg.vcf.header.VcfHeader;
+
+/**
+ * Provides GT merging that takes a majority vote at sample variants are genotyped multiple times.
+ */
+public class VcfGtMajorityMerger extends VcfRecordMerger {
+
+  private static final String DIP_MISSING = "./.";
+
+  /**
+   * Constructor
+   */
+  public VcfGtMajorityMerger() {
+    super(VcfUtils.FORMAT_GENOTYPE, false);
+  }
+
+
+  @Override
+  protected boolean mergeSamples(VcfRecord[] records, VcfHeader[] headers, VcfRecord dest, VcfHeader destHeader, AlleleMap map, Set<String> unmergeableFormatFields, boolean dropUnmergeable) {
+    final ArrayList<String> sampleGts = new ArrayList<>();
+    dest.getFormatAndSample().put(VcfUtils.FORMAT_GENOTYPE, sampleGts);
+    dest.setNumberOfSamples(destHeader.getNumberOfSamples());
+    final List<String> names = destHeader.getSampleNames();
+    if (map.mAltsChanged) {
+      // This is interesting, dump the situation:
+      Diagnostic.developerLog("INTERESTING (alts changed)");
+      for (VcfRecord r : records) {
+        Diagnostic.developerLog("REC: " + r.toString());
+      }
+    }
+    // For each sample we are going to output
+    for (int destSampleIndex = 0; destSampleIndex < names.size(); ++destSampleIndex) {
+      final MultiSet<String> counts = new MultiSet<>();
+      // Count the genotypes from the input records
+      for (int i = 0; i < headers.length; ++i) {
+        final int sampleIndex = headers[i].getSampleNames().indexOf(names.get(destSampleIndex));
+        if (sampleIndex > -1 && records[i].hasFormat(VcfUtils.FORMAT_GENOTYPE)) {
+
+          // Get the GT, remap alleles, and normalize allele ordering
+          final VcfRecord record = records[i];
+          final String gtStr = record.getFormat(VcfUtils.FORMAT_GENOTYPE).get(sampleIndex);
+          int[] splitGt = map.splitRemapGt(gtStr, record, i);
+
+          if (!VcfUtils.isMissingGt(splitGt)) {
+            Arrays.sort(splitGt); //  Ensure that het 0/1 vs 1/0 don't cause apparent discordance
+            counts.add(VcfUtils.joinGt(false, splitGt));
+          }
+        }
+      }
+      if (counts.keySet().isEmpty()) {
+        sampleGts.add(DIP_MISSING);
+      } else {
+        if (counts.keySet().size() > 1) {
+          Diagnostic.developerLog("Discordant genotypes found at " + new SequenceNameLocusSimple(dest) + " for sample " + names.get(destSampleIndex) + " " + counts);// Log the distribution?
+        }
+        String maxElement = null;
+        int maxCount = -1;
+        for (String genotype : counts.keySet()) {
+          int c = counts.get(genotype);
+          if (c > maxCount) {
+            maxElement = genotype;
+            maxCount = c;
+          }
+        }
+        if ((double) maxCount / counts.totalCount() > 0.75) {
+          sampleGts.add(maxElement);
+        } else {
+          sampleGts.add(DIP_MISSING);
+        }
+      }
+    }
+    return true;
+  }
+}
--- src/com/rtg/vcf/VcfMerge.java
+++ src/com/rtg/vcf/VcfMerge.java
@@ -85,6 +85,7 @@ public class VcfMerge extends AbstractCli {
   private static final String PRESERVE_FORMATS = "preserve-formats";
   private static final String NON_PADDING_AWARE = "Xnon-padding-aware";
   private static final String STATS_FLAG = "stats";
+  private static final String GT_MAJORITY = "Xgt-majority";
 
   @Override
   public String moduleName() {
@@ -112,6 +113,7 @@ public class VcfMerge extends AbstractCli {
     mFlags.registerOptional(PRESERVE_FORMATS, "if set, variants with different ALTs and unmergeable FORMAT fields will be kept unmerged (Default is to remove those FORMAT fields so the variants can be combined)").setCategory(UTILITY);
     mFlags.registerOptional(STATS_FLAG, "output statistics for the merged VCF file").setCategory(UTILITY);
     mFlags.registerOptional(NON_PADDING_AWARE, "allow merging of records that mix whether they employ a VCF anchor base").setCategory(UTILITY);
+    mFlags.registerOptional(GT_MAJORITY, "alternate mode that combines per sample GTs by majority vote").setCategory(UTILITY);
 
     mFlags.addRequiredSet(inFlag);
     mFlags.addRequiredSet(listFlag);
@@ -201,7 +203,8 @@ public class VcfMerge extends AbstractCli {
 
     final boolean stdout = FileUtils.isStdio(outFile);
     final File vcfFile = VcfUtils.getZippedVcfFileName(gzip, outFile);
-    final VcfRecordMerger merger = new VcfRecordMerger(defaultFormat, paddingAware);
+    final boolean gtMajority = mFlags.isSet(GT_MAJORITY);
+    final VcfRecordMerger merger = gtMajority ? new VcfGtMajorityMerger() : new VcfRecordMerger(defaultFormat, paddingAware);
     try (final VcfWriter w = new VcfWriterFactory(mFlags).addRunInfo(true).make(header, vcfFile)) {
       final ZipperCallback callback = (records, headers) -> {
         assert records.length > 0;
