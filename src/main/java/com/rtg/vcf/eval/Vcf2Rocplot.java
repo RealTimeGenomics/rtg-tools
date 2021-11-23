0f8901ac2ece660066c912ce4b99bc94adf793ef
Len Trigg <len@realtimegenomics.com>
2021-11-23 14:10:51 +1300

    Refactoring

--- src/main/java/com/rtg/vcf/eval/Vcf2Rocplot.java
+++ src/main/java/com/rtg/vcf/eval/Vcf2Rocplot.java
@@ -42,7 +42,6 @@ import static com.rtg.vcf.eval.VcfEvalCli.SORT_ORDER;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Collection;
 import java.util.List;
 
 import com.rtg.launcher.CommonFlags;
@@ -111,31 +110,40 @@ public class Vcf2Rocplot extends LoggedCli {
     // Create ROC container / extractor
     final RocSortOrder sortOrder = (RocSortOrder) mFlags.getValue(SORT_ORDER);
     mRocExtractor = RocSortValueExtractor.getRocSortValueExtractor((String) mFlags.getValue(SCORE_FIELD), sortOrder);
-    mRoc = new RocContainer(mRocExtractor);
+    final RocPointCriteria criteria;
     if (mFlags.isSet(CRITERIA_PRECISION)) {
-      mRoc.setRocPointCriteria(new PrecisionThreshold((Double) mFlags.getValue(CRITERIA_PRECISION)));
+      criteria = new PrecisionThreshold((Double) mFlags.getValue(CRITERIA_PRECISION));
     } else if (mFlags.isSet(CRITERIA_SENSITIVITY)) {
-      mRoc.setRocPointCriteria(new SensitivityThreshold((Double) mFlags.getValue(CRITERIA_SENSITIVITY)));
+      criteria = new SensitivityThreshold((Double) mFlags.getValue(CRITERIA_SENSITIVITY));
     } else if (mFlags.isSet(CRITERIA_SCORE)) {
-      mRoc.setRocPointCriteria(new FixedScoreThreshold((Double) mFlags.getValue(CRITERIA_SCORE), sortOrder.comparator()));
+      criteria = new FixedScoreThreshold((Double) mFlags.getValue(CRITERIA_SCORE), sortOrder.comparator());
+    } else {
+      criteria = new FMeasureThreshold();
     }
+    mRoc = new RocContainer(mRocExtractor);
+    mRoc.setRocPointCriteria(criteria);
     mRoc.addFilters(VcfEvalCli.getRocFilters(mFlags));
 
-    final Collection<?> values = mFlags.getAnonymousValues(0);
-    for (final Object o : values) {
+    final EvaluatedVariantsLoader[] loaders = {
+      new WithInfoVcfLoader(),
+      new Ga4ghVcfLoader()
+    };
+    for (final Object o : mFlags.getAnonymousValues(0)) {
+      boolean loaded = false;
       final File vcf = (File) o;
       final VcfReaderFactory fact = new VcfReaderFactory(mFlags);
       try (final VcfIterator vr = fact.make(vcf)) {
-        if (vr.getHeader().getInfoField(WithInfoEvalSynchronizer.INFO_CALL) != null
-          || vr.getHeader().getInfoField(WithInfoEvalSynchronizer.INFO_BASE) != null) {
-          Diagnostic.userLog("VCF file " + vcf + " looks to contain regular vcfeval annotation");
-          loadWithInfoVariants(vr, vcf);
-        } else if (vr.getHeader().getInfoField(Ga4ghEvalSynchronizer.FORMAT_DECISION) != null) {
-          Diagnostic.userLog("VCF file " + vcf + " looks to contain GA4GH annotations");
-          loadGa4ghVariants(vr);
-        } else {
-          Diagnostic.userLog("VCF file " + vcf + " doesn't look to contain any recognized annotations");
+        for (EvaluatedVariantsLoader l : loaders) {
+          if (l.isCompatible(vr.getHeader())) {
+            Diagnostic.userLog("VCF file " + vcf + " can be parsed as " + l);
+            l.loadVariants(vr, vcf);
+            loaded = true;
+            break;
+          }
         }
+      }
+      if (!loaded) {
+        Diagnostic.userLog("VCF file " + vcf + " doesn't look to contain any recognized annotations");
       }
     }
 
@@ -146,105 +154,139 @@ public class Vcf2Rocplot extends LoggedCli {
     return 0;
   }
 
-
-  private void loadWithInfoVariants(VcfIterator vr, File vcf) throws IOException {
-    final VcfHeader header = vr.getHeader();
-    // If the VCF is from combined output, look up the appropriate sample indexes, otherwise assume 0
-    final int bSample = getSampleIndex(header, CombinedEvalSynchronizer.SAMPLE_BASELINE);
-    final int cSample = getSampleIndex(header, CombinedEvalSynchronizer.SAMPLE_CALLS);
-    final boolean isCombined = bSample != 0 || cSample != 0;
-    if (mRocExtractor.requiresSample()) {
-      if (!isCombined && header.getNumberOfSamples() > 1) {
-        Diagnostic.warning("VCF file " + vcf + " contains multiple samples, assuming first");
-        // If this is wrong, the user should vcfsubset to pick out the sample they want.
-      }
-    }
-    mRoc.filters().forEach(f -> f.setHeader(header));
-
-    while (vr.hasNext()) {
-      final VcfRecord rec = vr.next();
-
-      final String base = rec.getInfo(WithInfoEvalSynchronizer.INFO_BASE);
-      if (base != null) {
-        switch (base) {
-          case WithInfoEvalSynchronizer.STATUS_TP:
-            mRoc.incrementBaselineCount(rec, bSample, true);
-            break;
-          case WithInfoEvalSynchronizer.STATUS_FN:
-          case WithInfoEvalSynchronizer.STATUS_FN_CA:
-            mRoc.incrementBaselineCount(rec, bSample, false);
-            break;
-          case WithInfoEvalSynchronizer.STATUS_OUTSIDE:
-          case WithInfoEvalSynchronizer.STATUS_HARD:
-          case WithInfoEvalSynchronizer.STATUS_IGNORED:
-            // ignore these known values silently
-            break;
-          default:
-            Diagnostic.warning("Unrecognized annotation for " + WithInfoEvalSynchronizer.INFO_BASE + " " + base);
-            // ignore
-        }
-      }
-      final String call = rec.getInfo(WithInfoEvalSynchronizer.INFO_CALL);
-      if (call != null) {
-        switch (call) {
-          case WithInfoEvalSynchronizer.STATUS_TP:
-            final double weight = VcfUtils.getDoubleInfoFieldFromRecord(rec, WithInfoEvalSynchronizer.INFO_CALL_WEIGHT);
-            mRoc.addRocLine(rec, cSample, Double.isNaN(weight) ? 1 : weight, 0, 1);
-            break;
-          case WithInfoEvalSynchronizer.STATUS_FP:
-          case WithInfoEvalSynchronizer.STATUS_FP_CA:
-            mRoc.addRocLine(rec, cSample, 0, 1, 0);
-            break;
-          case WithInfoEvalSynchronizer.STATUS_OUTSIDE:
-          case WithInfoEvalSynchronizer.STATUS_HARD:
-          case WithInfoEvalSynchronizer.STATUS_IGNORED:
-            // ignore these known values silently
-            break;
-          default:
-            Diagnostic.warning("Unrecognized annotation for " + WithInfoEvalSynchronizer.INFO_CALL + " " + call);
-            // ignore
-        }
-      }
-    }
-  }
-
-  private void loadGa4ghVariants(VcfIterator vr) throws IOException {
-    while (vr.hasNext()) {
-      final VcfRecord rec = vr.next();
-
-      final List<String> decision = rec.getFormat(Ga4ghEvalSynchronizer.FORMAT_DECISION);
-      if (decision == null) {
-        continue;
-      }
-
-      switch (decision.get(Ga4ghEvalSynchronizer.TRUTH_SAMPLE_INDEX)) {
-        case Ga4ghEvalSynchronizer.DECISION_TP:
-          mRoc.incrementBaselineCount(rec, Ga4ghEvalSynchronizer.TRUTH_SAMPLE_INDEX, true);
-          break;
-        case Ga4ghEvalSynchronizer.DECISION_FN:
-          mRoc.incrementBaselineCount(rec, Ga4ghEvalSynchronizer.TRUTH_SAMPLE_INDEX, false);
-          break;
-        default:
-          // ignore
-      }
-      switch (decision.get(Ga4ghEvalSynchronizer.QUERY_SAMPLE_INDEX)) {
-        case Ga4ghEvalSynchronizer.DECISION_TP:
-          mRoc.addRocLine(rec, Ga4ghEvalSynchronizer.QUERY_SAMPLE_INDEX, 1, 0, 1);
-          break;
-        case Ga4ghEvalSynchronizer.DECISION_FP:
-          mRoc.addRocLine(rec, Ga4ghEvalSynchronizer.QUERY_SAMPLE_INDEX, 0, 1, 0);
-          break;
-        default:
-          // ignore
-      }
-    }
-  }
-
-
   // Find the named sample or 0 otherwise
-  private int getSampleIndex(VcfHeader header, String sample) {
+  private static int getSampleIndex(VcfHeader header, String sample) {
     final int index = header.getSampleIndex(sample);
     return index == -1 ? 0 : index;
+  }
+
+
+  private interface EvaluatedVariantsLoader {
+    boolean isCompatible(VcfHeader vh);
+    void loadVariants(VcfIterator vr, File vcf) throws IOException;
+  }
+
+
+  // Parses annotations created by vcfeval --output-mode annotate or --output-mode combine
+  private class WithInfoVcfLoader implements EvaluatedVariantsLoader {
+
+    public String toString() {
+      return "vcfeval standard annotations";
+    }
+
+    @Override
+    public boolean isCompatible(VcfHeader vh) {
+      return vh.getInfoField(WithInfoEvalSynchronizer.INFO_CALL) != null
+        || vh.getInfoField(WithInfoEvalSynchronizer.INFO_BASE) != null;
+    }
+
+    @Override
+    public void loadVariants(VcfIterator vr, File vcf) throws IOException {
+      final VcfHeader header = vr.getHeader();
+      // If the VCF is from combined output, look up the appropriate sample indexes, otherwise assume 0
+      final int bSample = getSampleIndex(header, CombinedEvalSynchronizer.SAMPLE_BASELINE);
+      final int cSample = getSampleIndex(header, CombinedEvalSynchronizer.SAMPLE_CALLS);
+      final boolean isCombined = bSample != 0 || cSample != 0;
+      if (mRocExtractor.requiresSample()) {
+        if (!isCombined && header.getNumberOfSamples() > 1) {
+          Diagnostic.warning("VCF file " + vcf + " contains multiple samples, assuming first");
+          // If this is wrong, the user should vcfsubset to pick out the sample they want.
+        }
+      }
+      mRoc.filters().forEach(f -> f.setHeader(header));
+
+      while (vr.hasNext()) {
+        final VcfRecord rec = vr.next();
+
+        final String base = rec.getInfo(WithInfoEvalSynchronizer.INFO_BASE);
+        if (base != null) {
+          switch (base) {
+            case WithInfoEvalSynchronizer.STATUS_TP:
+              mRoc.incrementBaselineCount(rec, bSample, true);
+              break;
+            case WithInfoEvalSynchronizer.STATUS_FN:
+            case WithInfoEvalSynchronizer.STATUS_FN_CA:
+              mRoc.incrementBaselineCount(rec, bSample, false);
+              break;
+            case WithInfoEvalSynchronizer.STATUS_OUTSIDE:
+            case WithInfoEvalSynchronizer.STATUS_HARD:
+            case WithInfoEvalSynchronizer.STATUS_IGNORED:
+              // ignore these known values silently
+              break;
+            default:
+              Diagnostic.warning("Unrecognized annotation for " + WithInfoEvalSynchronizer.INFO_BASE + " " + base);
+              // ignore
+          }
+        }
+        final String call = rec.getInfo(WithInfoEvalSynchronizer.INFO_CALL);
+        if (call != null) {
+          switch (call) {
+            case WithInfoEvalSynchronizer.STATUS_TP:
+              final double weight = VcfUtils.getDoubleInfoFieldFromRecord(rec, WithInfoEvalSynchronizer.INFO_CALL_WEIGHT);
+              mRoc.addRocLine(rec, cSample, Double.isNaN(weight) ? 1 : weight, 0, 1);
+              break;
+            case WithInfoEvalSynchronizer.STATUS_FP:
+            case WithInfoEvalSynchronizer.STATUS_FP_CA:
+              mRoc.addRocLine(rec, cSample, 0, 1, 0);
+              break;
+            case WithInfoEvalSynchronizer.STATUS_OUTSIDE:
+            case WithInfoEvalSynchronizer.STATUS_HARD:
+            case WithInfoEvalSynchronizer.STATUS_IGNORED:
+              // ignore these known values silently
+              break;
+            default:
+              Diagnostic.warning("Unrecognized annotation for " + WithInfoEvalSynchronizer.INFO_CALL + " " + call);
+              // ignore
+          }
+        }
+      }
+    }
+  }
+
+  // Parses annotations created by vcfeval --output-mode ga4gh
+  private class Ga4ghVcfLoader implements EvaluatedVariantsLoader {
+
+    public String toString() {
+      return "vcfeval ga4gh annotations";
+    }
+
+    @Override
+    public boolean isCompatible(VcfHeader vh) {
+      return vh.getInfoField(Ga4ghEvalSynchronizer.FORMAT_DECISION) != null;
+    }
+
+    @Override
+    public void loadVariants(VcfIterator vr, File vcf) throws IOException {
+      while (vr.hasNext()) {
+        final VcfRecord rec = vr.next();
+
+        final List<String> decision = rec.getFormat(Ga4ghEvalSynchronizer.FORMAT_DECISION);
+        if (decision == null) {
+          continue;
+        }
+
+        switch (decision.get(Ga4ghEvalSynchronizer.TRUTH_SAMPLE_INDEX)) {
+          case Ga4ghEvalSynchronizer.DECISION_TP:
+            mRoc.incrementBaselineCount(rec, Ga4ghEvalSynchronizer.TRUTH_SAMPLE_INDEX, true);
+            break;
+          case Ga4ghEvalSynchronizer.DECISION_FN:
+            mRoc.incrementBaselineCount(rec, Ga4ghEvalSynchronizer.TRUTH_SAMPLE_INDEX, false);
+            break;
+          default:
+            // ignore
+        }
+        switch (decision.get(Ga4ghEvalSynchronizer.QUERY_SAMPLE_INDEX)) {
+          case Ga4ghEvalSynchronizer.DECISION_TP:
+            mRoc.addRocLine(rec, Ga4ghEvalSynchronizer.QUERY_SAMPLE_INDEX, 1, 0, 1);
+            break;
+          case Ga4ghEvalSynchronizer.DECISION_FP:
+            mRoc.addRocLine(rec, Ga4ghEvalSynchronizer.QUERY_SAMPLE_INDEX, 0, 1, 0);
+            break;
+          default:
+            // ignore
+        }
+      }
+    }
   }
 
 }
