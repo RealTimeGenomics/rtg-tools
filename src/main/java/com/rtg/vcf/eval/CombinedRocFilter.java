c8aeeace9d414db9cbedfd368e7c5728da9aba30
rickymagner <81349869+rickymagner@users.noreply.github.com>
2023-10-13 08:52:08 -0400

    Add CombinedRocFilter and ROC_INTERSECT_MODE to vcfeval

---
+++ src/main/java/com/rtg/vcf/eval/CombinedRocFilter.java
@@ -0,0 +1,39 @@
+package com.rtg.vcf.eval;
+
+import com.rtg.vcf.VcfRecord;
+import com.rtg.vcf.header.VcfHeader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinedRocFilter extends RocFilter {
+    private final ArrayList<RocFilter> mRocFilters = new ArrayList<>();
+
+    /**
+     * Create a CombinedRocFilter which incorporates multiple RocFilters into one filter
+     * @param inputRocFilters List of RocFilters matched against to accept variant
+     */
+    public CombinedRocFilter(List<RocFilter> inputRocFilters, Boolean rescale) {
+        super("", rescale);
+        final ArrayList<String> names = new ArrayList<>();
+
+        for (RocFilter rocFilter : inputRocFilters) {
+            if (rocFilter != RocFilter.ALL) {
+                mRocFilters.add(rocFilter);
+                names.add(rocFilter.name());
+            }
+        }
+
+        mName = String.join("+", names);
+    }
+
+    @Override
+    public void setHeader(VcfHeader header) {
+        mRocFilters.forEach(f -> f.setHeader(header));
+    }
+
+    @Override
+    public boolean accept(VcfRecord rec, int[] gt) {
+        return mRocFilters.stream().allMatch(f -> f.accept(rec, gt));
+    }
+}
